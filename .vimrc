1a4eb89f105517658f75350324a7c14180a267f2
datle171095 <datle171095@gmail.com>
2016-09-11 23:43:15 +0700

    First commit.

---
+++ .vimrc
@@ -0,0 +1,75 @@
+set nocompatible              
+filetype off                  
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'morhetz/gruvbox'
+Plugin 'vim-airline/vim-airline'
+Plugin 'vim-airline/vim-airline-themes'
+Plugin 'PotatoesMaster/i3-vim-syntax'
+call vundle#end()            
+filetype plugin indent on   
+filetype plugin on
+syntax on
+
+let g:airline_theme='gruvbox'
+let g:airline_powerline_fonts=1
+
+let g:gruvbox_contrast_dark='medium'
+colorscheme gruvbox
+set background=dark
+
+set laststatus=2
+set noshowmode
+
+set showcmd
+set ignorecase
+set smartcase
+set expandtab
+set smarttab
+set hlsearch
+set number
+set noswapfile
+set cursorline
+set autoindent
+
+set shiftwidth=4
+set softtabstop=4
+set scrolloff=8
+set sidescrolloff=10
+
+set timeoutlen=1000
+set ttimeoutlen=0
+
+set clipboard=unnamed
+set clipboard^=unnamedplus 
+
+:noremap / :set hlsearch<CR>/
+
+" toggle paste mode
+:noremap <F2> :set paste! nopaste?<CR>
+
+" toggle number lines
+:noremap <F3> :set nonumber! nonumber?<CR>
+
+" toggle search highlights
+:noremap <F4> :set hlsearch! hlsearch?<CR>
+
+function TrimWhitespace()
+  %s/\s*$//
+  ''
+:endfunction
+command! Trim call TrimWhitespace()
+
+" Shows the highlight group of whatever's under the cursor
+ map <F10> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<'
+ \ . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
+ \ . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">"<CR>
+
+set hidden
+let mapleader=","
+nmap <leader>n :enew<cr>
+nmap <leader><leader> :bnext<CR>
+nmap <leader>. :bprevious<CR>
+nmap <leader>l :ls<CR>
---
+++ awesome/themes/multicolor/theme.lua
@@ -0,0 +1,108 @@
+
+--[[
+                                      
+     Multicolor Awesome WM config 2.0 
+     github.com/copycat-killer        
+                                      
+--]]
+
+
+theme                               = {}
+
+theme.confdir                       = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
+theme.wallpaper                     = theme.confdir .. "/octocat.jpg"
+
+theme.font                          = "Terminus 8"
+--theme.taglist_font                =
+theme.menu_bg_normal                = "#000000"
+theme.menu_bg_focus                 = "#000000"
+theme.bg_normal                     = "#000000"
+theme.bg_focus                      = "#000000"
+theme.bg_urgent                     = "#000000"
+theme.fg_normal                     = "#aaaaaa"
+theme.fg_focus                      = "#ff8c00"
+theme.fg_urgent                     = "#af1d18"
+theme.fg_minimize                   = "#ffffff"
+theme.fg_black                      = "#424242"
+theme.fg_red                        = "#ce5666"
+theme.fg_green                      = "#80a673"
+theme.fg_yellow                     = "#ffaf5f"
+theme.fg_blue                       = "#7788af"
+theme.fg_magenta                    = "#94738c"
+theme.fg_cyan                       = "#778baf"
+theme.fg_white                      = "#aaaaaa"
+theme.fg_blu                        = "#8ebdde"
+theme.border_width                  = "1"
+theme.border_normal                 = "#1c2022"
+theme.border_focus                  = "#606060"
+theme.border_marked                 = "#3ca4d8"
+theme.menu_width                    = "110"
+theme.menu_border_width             = "0"
+theme.menu_fg_normal                = "#aaaaaa"
+theme.menu_fg_focus                 = "#ff8c00"
+theme.menu_bg_normal                = "#050505dd"
+theme.menu_bg_focus                 = "#050505dd"
+--theme.bg_systray = "#12121290"
+
+theme.submenu_icon                  = theme.confdir .. "/icons/submenu.png"
+theme.widget_temp                   = theme.confdir .. "/icons/temp.png"
+--theme.widget_uptime                 = theme.confdir .. "/icons/ac.png"
+theme.widget_cpu                    = theme.confdir .. "/icons/cpu.png"
+theme.widget_weather                = theme.confdir .. "/icons/dish.png"
+theme.widget_fs                     = theme.confdir .. "/icons/fs.png"
+theme.widget_mem                    = theme.confdir .. "/icons/mem.png"
+theme.widget_note                   = theme.confdir .. "/icons/note.png"
+theme.widget_note_on                = theme.confdir .. "/icons/note_on.png"
+theme.widget_netdown                = theme.confdir .. "/icons/net_down.png"
+theme.widget_netup                  = theme.confdir .. "/icons/net_up.png"
+theme.widget_mail                   = theme.confdir .. "/icons/mail.png"
+theme.widget_clock                  = theme.confdir .. "/icons/clock.png"
+theme.awesome_icon                  = theme.confdir .. "/icons/awesome.png"
+
+theme.widget_vol                    = theme.confdir .. "/icons/vol.png"
+theme.widget_vol_low                = theme.confdir .. "/icons/vol_low.png"
+theme.widget_vol_no                 = theme.confdir .. "/icons/vol_no.png"
+theme.widget_vol_mute               = theme.confdir .. "/icons/vol_mute.png"
+
+theme.widget_ac                     = theme.confdir .. "/icons/ac.png"
+theme.widget_battery                = theme.confdir .. "/icons/battery.png"
+theme.widget_battery_low            = theme.confdir .. "/icons/battery_low.png"
+theme.widget_battery_empty          = theme.confdir .. "/icons/battery_empty.png"
+
+theme.taglist_squares_sel           = theme.confdir .. "/icons/square_a.png"
+theme.taglist_squares_unsel         = theme.confdir .. "/icons/square_b.png"
+
+theme.tasklist_disable_icon         = true
+theme.tasklist_floating             = ""
+theme.tasklist_maximized_horizontal = ""
+theme.tasklist_maximized_vertical   = ""
+
+theme.layout_tile                   = theme.confdir .. "/icons/tile.png"
+theme.layout_tilegaps               = theme.confdir .. "/icons/tilegaps.png"
+theme.layout_tileleft               = theme.confdir .. "/icons/tileleft.png"
+theme.layout_tilebottom             = theme.confdir .. "/icons/tilebottom.png"
+theme.layout_tiletop                = theme.confdir .. "/icons/tiletop.png"
+theme.layout_fairv                  = theme.confdir .. "/icons/fairv.png"
+theme.layout_fairh                  = theme.confdir .. "/icons/fairh.png"
+theme.layout_spiral                 = theme.confdir .. "/icons/spiral.png"
+theme.layout_dwindle                = theme.confdir .. "/icons/dwindle.png"
+theme.layout_max                    = theme.confdir .. "/icons/max.png"
+theme.layout_fullscreen             = theme.confdir .. "/icons/fullscreen.png"
+theme.layout_magnifier              = theme.confdir .. "/icons/magnifier.png"
+theme.layout_floating               = theme.confdir .. "/icons/floating.png"
+
+-- lain related
+theme.useless_gap_width		    = 7
+theme.layout_centerfair		    = theme.confdir .. "/icons/centerfair.png"
+theme.layout_termfair		    = theme.confdir .. "/icons/termfair.png"
+theme.layout_centerwork		    = theme.confdir .. "/icons/centerwork.png"
+theme.layout_uselessfair	    = theme.confdir .. "/icons/fairv.png"
+theme.layout_uselessfairh	    = theme.confdir .. "/icons/fairh.png"
+theme.layout_uselessdwindle	    = theme.confdir .. "/icons/dwindle.png"
+theme.layout_uselesstile	    = theme.confdir .. "/icons/tile.png"
+theme.layout_uselesstiletop	    = theme.confdir .. "/icons/tiletop.png"
+theme.layout_uselesstileleft	    = theme.confdir .. "/icons/tileleft.png"
+theme.layout_uselesstilebottom	    = theme.confdir .. "/icons/tilebottom.png"
+theme.layout_uselesspiral	    = theme.confdir .. "/icons/spiral.png"
+
+return theme
